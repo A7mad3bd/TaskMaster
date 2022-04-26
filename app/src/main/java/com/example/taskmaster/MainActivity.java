945a0c59261baac8c60da35a04ff545a2f40a9ef
A7mad3bd <ahmadabd398@gmail.com>
2022-04-26 03:53:32 +0300

    finished

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -9,28 +9,25 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-
-    private final View.OnClickListener mClickMeButtonListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            mGreetingText.setAllCaps(true);
-
-            // create intent for navigation
-            Intent startSecondActivityIntent = new Intent(getApplicationContext(), AllTasks.class);
-            // create intent for navigation
-            Intent startAddingTaskIntent = new Intent(getApplicationContext(), AddingTask.class);
-
-
-            // start / navigate to the next activity
-            startActivity(startSecondActivityIntent);
-            startActivity(startAddingTaskIntent);
-
-        }
-    };
-    private TextView mGreetingText;
+    private Button button,button1;
+//    private final View.OnClickListener mClickMeButtonListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            mGreetingText.setAllCaps(true);
+//
+//            // create intent for navigation
+////            Intent startSecondActivityIntent = new Intent(getApplicationContext(), AddingTask.class);
+////
+////            // start / navigate to the next activity
+////            startActivity(startSecondActivityIntent);
+//        }
+//    };
+//    private TextView mGreetingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +37,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate: Called");
 
-        // get access to the objects inflated from activity_main
+        button = (Button) findViewById(R.id.ADDTASK);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
 
+
+        button1 = (Button) findViewById(R.id.AllTasks);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity1();
+            }
+        });
+    }
+
+    public void openActivity1() {
+        Intent intent = new Intent(this, AllTasks.class);
+        startActivity(intent);
+    }
+    public void openActivity2() {
+        Intent intent = new Intent(this, AddingTask.class);
+        startActivity(intent);
     }
 
     @Override
@@ -79,5 +99,17 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "onDestroy: called");
         super.onDestroy();
     }
+//*
+//private Button button;
+//        button = (Button) findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(this, Activity2.class);
+////        startActivity(intent);
+//            }
+//        });
+//    }
 
+// */
 }

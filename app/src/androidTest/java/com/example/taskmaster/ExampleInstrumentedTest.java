6af873c1ed2c3aed2d71dc13cb2fce871408c5f5
A7mad3bd <ahmadabd398@gmail.com>
2022-05-21 18:03:59 +0300

    Lab29

--- app/src/androidTest/java/com/example/taskmaster/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/taskmaster/ExampleInstrumentedTest.java
@@ -2,25 +2,79 @@ package com.example.taskmaster;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.Espresso;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-
+import androidx.test.platform.app.InstrumentationRegistry;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import androidx.test.rule.ActivityTestRule;
 
-import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivity =
+            new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.taskmaster", appContext.getPackageName());
     }
+
+    @Test
+    public void AddTaskTest() {
+        RecyclerView recyclerView = mainActivity.getActivity().findViewById(R.id.rere);
+        int itemCount1 = recyclerView.getAdapter().getItemCount();
+        Espresso.onView(withId(R.id.ADDTASK)).perform(click());
+        onView(withId(R.id.Tasknamee)).perform(typeText("Add Task1234"), closeSoftKeyboard());
+        onView(withId(R.id.Taskbodyy)).perform(typeText("espresso test1"), closeSoftKeyboard());
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.ADDTASKA)).perform(click());
+        int itemCount2 = recyclerView.getAdapter().getItemCount();
+        System.out.println(itemCount1);
+        System.out.println(itemCount2);
+    }
+    @Test
+    public void EditUsernameSettingsTest() {
+        Espresso.onView(withId(R.id.Settings)).perform(click());
+        onView(withId(R.id.Write_your_username)).perform(typeText("User Test"), closeSoftKeyboard());
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.btn_submit)).perform(click());
+        onView(withId(R.id.username)).check(matches(withText("User Test")));
+        Espresso.pressBack();
+
+    }
+    @Test
+    public void TaskClickTest() {
+        onView(withId(R.id.rere)).perform(actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.Task_name)).check(matches(withText("new")));
+        onView(withId(R.id.Task_Body)).check(matches(withText("Do ")));
+    }
+
+    @Test
+    public void UI_elementsTest() {
+
+        onView(withId(R.id.Settings)).check(matches(isDisplayed()));
+        onView(withId(R.id.ADDTASK)).check(matches(isDisplayed()));
+        onView(withId(R.id.rere)).check(matches(isDisplayed()));
+        onView(withId(R.id.username)).check(matches(isDisplayed()));
+    }
+
+
+
+
 }
